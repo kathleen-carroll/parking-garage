21899dc2f5abe21c8c473411c2c4dbef2dce74bc
Kathleen Carroll <khakicarroll@comcast.net>
2020-12-08 11:21:04 -0700

    Build out level to have an array of rows

--- lib/level.rb
+++ lib/level.rb
@@ -1,5 +1,9 @@
 class Level
-  def initialize
-    @rows = []
+  attr_reader :floor, :rows
+
+  def initialize(floor, rows)
+    @floor = floor
+    @rows = rows
   end
+
 end
--- test/level_test.rb
+++ test/level_test.rb
@@ -7,16 +7,28 @@ require './lib/level'
 
 class LevelTest < Minitest::Test
   def setup
-    @level = Level.new
-    # @row1 = Row.new
-    # spot1 = Spot.new('compact')
-    # spot2 = Spot.new('motorcycle')
-    # spot3 = Spot.new('large')
-    # @spots = [spot1, spot2, spot3]
-    # @row2 = Row.new(2, @spots)
+    @row1 = Row.new
+    spot1 = Spot.new('compact')
+    spot2 = Spot.new('motorcycle')
+    spot3 = Spot.new('large')
+    @spots = [spot1, spot2, spot3]
+    @row2 = Row.new(2, @spots)
+    @rows = [@row1, @row2]
+    @level1 = Level.new(1, @rows)
   end
 
   def test_it_exists
-    assert_instance_of Level, @level
+    assert_instance_of Level, @level1
+  end
+
+  def test_it_has_a_floor_num_and_rows
+    assert_equal @level1.floor, 1
+    assert_equal @level1.rows, @rows
+  end
+
+  def test_it_can_find_empty_spots
+  end
+
+  def test_it_can_find_parked_cars
   end
 end
